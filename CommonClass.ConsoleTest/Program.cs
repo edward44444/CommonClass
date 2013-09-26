f51080c79deaa355da339823799721934d131c3c
edward <edward44442010@hotmial.com>
2013-09-26 23:05:41 +0800

    HttpHelper add UploadFile method

--- CommonClass.ConsoleTest/Program.cs
+++ CommonClass.ConsoleTest/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Drawing;
 using System.Threading;
 using System.Web;
+using System.Collections.Specialized;
 
 namespace CommonClass.ConsoleTest
 {
@@ -14,34 +15,39 @@ namespace CommonClass.ConsoleTest
 
         static void Main(string[] args)
         {
-            //HttpHelper httpHelper = new HttpHelper("GET");
-            //Console.WriteLine(httpHelper.GetResponse("http://www.baidu.com/"));
-
+            HttpHelperTest();
             //WebPageCapturer caputer = new WebPageCapturer(1024, 768);
             //Bitmap image = caputer.Capture("http://www.baidu.com/",3000);
             //image.Save("1.jpg");
 
-            //CustomCacheDependencyManager.Start();
-            //new Thread(() => {
-            //    int i=0;
-            //    while (true)
-            //    {
-            //        if (_cahe.Get("DateTime") == null)
-            //        {
-            //            CustomCacheDependency dep = new CustomCacheDependency("edward");
-            //            Console.WriteLine(dep.HasChanged);
-            //            Console.WriteLine(dep.UtcLastModified.ToString("yyyy-MM-dd HH:mm:ss"));
-            //            _cahe.Insert("DateTime", DateTime.Now,dep );
-            //        }
-            //        if (_cahe.Get("DateTime") != null)
-            //        {
-            //            Console.WriteLine(((DateTime)_cahe.Get("DateTime")).ToString("yyyyMMddHHmmss"));
-            //        }
-            //        Thread.Sleep(1000);
-            //        i++;
-            //    }
-            //}).Start();
+            //CommonCacheDependencyTest();
 
+
+            Console.Read();
+        }
+
+        private static void HttpHelperTest()
+        {
+            //HttpHelper httpHelper = new HttpHelper("GET");
+            //Console.WriteLine(httpHelper.GetResponse("http://www.baidu.com/"));
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            HttpHelper httpHelper = new HttpHelper("POST", "multipart/form-data; boundary=----WebKitFormBoundaryYFFgMi35KsmiB8ki", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", headers);
+            httpHelper.KeepAlive = true;
+            NameValueCollection form = new NameValueCollection();
+            form.Add("__VIEWSTATE", "/wEPDwUKMTUyOTIzOTY3M2RklcwzkiuxMWwLbqK2j+X8v1uEapS2vVQi8rhcQKUbwZE=");
+            form.Add("__EVENTVALIDATION", "/wEdAAIwq61+ETXzKZgFsCRK53u75vhDofrSSRcWtmHPtJVt4FCaPsncA3ve5V4+UUACO0qRaDoWAJ1vUeQbi2jS6ynN");
+            NameValueCollection controlForm = new NameValueCollection();
+            controlForm.Add("btnUpload", "Upload");
+            UploadFileInfo[] files = new UploadFileInfo[]
+            {
+                new UploadFileInfo{FilePath=@"C:\Users\Public\Pictures\Sample Pictures\dog.jpg",ControlName="file1",ContentType="image/jpeg"},
+                new UploadFileInfo{FilePath=@"C:\Users\Public\Pictures\Sample Pictures\123.jpg",ControlName="file2",ContentType="image/jpeg"}
+            };
+            Console.WriteLine(httpHelper.UploadFile("http://edward-pc:5566/FileUploader.aspx", form, controlForm, files));
+        }
+
+        private static void CommonCacheDependencyTest()
+        {
             new Thread(() =>
             {
                 int i = 0;
@@ -56,13 +62,12 @@ namespace CommonClass.ConsoleTest
                     }
                     if (_cahe.Get("DateTime") != null)
                     {
-                        Console.WriteLine("edward"+((DateTime)_cahe.Get("DateTime")).ToString("yyyyMMddHHmmss"));
+                        Console.WriteLine("edward" + ((DateTime)_cahe.Get("DateTime")).ToString("yyyyMMddHHmmss"));
                     }
                     Thread.Sleep(1000);
                     i++;
                 }
             }).Start();
-            Console.Read();
         }
     }
 }
--- CommonClass/HttpHelper.cs
+++ CommonClass/HttpHelper.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Collections.Specialized;
 
 namespace CommonClass
 {
+    public class UploadFileInfo
+    {
+        public string FilePath { get; set; }
+
+        public string ControlName { get; set; }
+
+        public string ContentType { get; set; }
+    }
+
     public class HttpHelper
     {
         private string _method;
@@ -28,6 +38,20 @@ namespace CommonClass
         {
             get { return _accept; }
             set { _accept = value; }
+        }
+
+        private bool _keepAlive;
+        public bool KeepAlive
+        {
+            get { return _keepAlive; }
+            set { _keepAlive = value; }
+        }
+
+        private string _referer;
+        public string Referer
+        {
+            get { return _referer; }
+            set { _referer = value; }
         }
 
         private Dictionary<string, string> _headers;
@@ -65,9 +89,11 @@ namespace CommonClass
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(uri));
             request.AllowAutoRedirect = _allowAutoRedirect;
             request.Method = _method;
+            if (_keepAlive == true) request.KeepAlive = _keepAlive;
             if (_porxy != null) request.Proxy = _porxy;
             if (!string.IsNullOrEmpty(_contentType)) request.ContentType = _contentType;
             if (!string.IsNullOrEmpty(_accept)) request.Accept = _accept;
+            if (!string.IsNullOrEmpty(_referer)) request.Referer = _referer;
             if (cookieContainer != null) request.CookieContainer = cookieContainer;
             if (_headers != null)
             {
@@ -87,15 +113,80 @@ namespace CommonClass
             return request;
         }
 
+        private HttpWebRequest GetRequest(string uri, NameValueCollection form, NameValueCollection controlForm, UploadFileInfo[] files, CookieContainer cookieContainer)
+        {
+            string boundary = _contentType.Substring(_contentType.IndexOf("boundary=") + "boundary=".Length);
+            byte[] boundaryBytes = Encoding.UTF8.GetBytes("\r\n--" + boundary + "\r\n");
+            byte[] endLineBytes = Encoding.UTF8.GetBytes("\r\n--" + boundary + "--\r\n");
+            string formTemplate = "\r\n--" + boundary + "\r\nContent-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            string fileHeaderTemplate = "\r\n--" + boundary + "\r\nContent-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            HttpWebRequest request = GetRequest(uri, null, cookieContainer);
+            request.SendChunked = false;
+            request.ServicePoint.Expect100Continue = false;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                if (form != null)
+                {
+                    for (int i = 0; i < form.Keys.Count; i++)
+                    {
+                        string key = form.Keys[i];
+                        string formItem = string.Format(formTemplate, key, form[key]);
+                        byte[] formItemBytes = Encoding.UTF8.GetBytes(formItem);
+                        stream.Write(formItemBytes, 0, formItemBytes.Length);
+                    }
+                }
+                foreach (var file in files)
+                {
+                    string fileHeader = string.Format(fileHeaderTemplate, file.ControlName, Path.GetFileName(file.FilePath), file.ContentType);
+                    byte[] fileHeaderBytes = Encoding.UTF8.GetBytes(fileHeader);
+                    stream.Write(fileHeaderBytes, 0, fileHeaderBytes.Length);
+                    using (FileStream fs = new FileStream(file.FilePath, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] buffer = new byte[1024];
+                        int readBytes = 0;
+                        while ((readBytes = fs.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            stream.Write(buffer, 0, readBytes);
+                        }
+                    }
+                }
+                if (controlForm != null)
+                {
+                    for (int i = 0; i < controlForm.Keys.Count; i++)
+                    {
+                        string key = controlForm.Keys[i];
+                        string formItem = string.Format(formTemplate, key, controlForm[key]);
+                        byte[] formItemBytes = Encoding.UTF8.GetBytes(formItem);
+                        stream.Write(formItemBytes, 0, formItemBytes.Length);
+                    }
+                }
+                stream.Write(boundaryBytes, 0, boundaryBytes.Length);
+                request.ContentLength = stream.Length;
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    stream.Position = 0;
+                    stream.CopyTo(requestStream);
+                }
+            }
+            return request;
+        }
+
         public string GetResponse(string uri, byte[] postData = null, CookieContainer cookieContainer = null)
         {
             HttpWebRequest request = GetRequest(uri, postData, cookieContainer);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            using (Stream stream = response.GetResponseStream())
+            return ExecResponseString(request);
+        }
+
+        private string ExecResponseString(HttpWebRequest request)
+        {
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
             {
-                using (StreamReader sr = new StreamReader(stream))
+                using (Stream stream = response.GetResponseStream())
                 {
-                   return sr.ReadToEnd();
+                    using (StreamReader sr = new StreamReader(stream))
+                    {
+                        return sr.ReadToEnd();
+                    }
                 }
             }
         }
@@ -105,6 +196,12 @@ namespace CommonClass
             HttpWebRequest request = GetRequest(uri, postData, cookieContainer);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             return response;
+        }
+
+        public string UploadFile(string uri, NameValueCollection form, NameValueCollection controlForm, UploadFileInfo[] files, CookieContainer cookieContainer = null)
+        {
+            HttpWebRequest request = GetRequest(uri, form, controlForm, files, cookieContainer);
+            return ExecResponseString(request);
         }
     }
 }
